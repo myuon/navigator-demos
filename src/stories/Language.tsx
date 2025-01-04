1a5541be279477278f69f8164e2a854849629d10
myuon <ioi.joi.koi.loi@gmail.com>
2025-01-04 18:56:43 +0900

    feat: languages

--- src/stories/Language.tsx
+++ src/stories/Language.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+
 export const Language: React.FC = () => {
+  const [log, setLog] = useState<string[]>([]);
+  useEffect(() => {
+    window.addEventListener("languagechange", () => {
+      setLog((prev) => [
+        ...prev,
+        `${new Date()} Language changed to ${navigator.language}`,
+      ]);
+    });
+  }, []);
+
   return (
     <div>
       <p>Language: {navigator.language}</p>
+
+      <p>Languages: {navigator.languages.join(", ")}</p>
+
+      <textarea
+        value={log.join("\n")}
+        readOnly
+        style={{ width: "100%", height: "200px" }}
+      ></textarea>
     </div>
   );
 };
